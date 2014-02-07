2ad2a0b231fff6d26699555d93759b2a4a56b78e
Gaurav Saxena <gsaxena81@gmail.com>
2014-02-06 20:53:44 -0800

    Assignment 2

---
+++ Assignment 2/featureFunction.m
@@ -0,0 +1,13 @@
+function x = featureFunction()
+    x = {@IngWords, @InterrogativeWords, @ExclamatoryWords, @ConnectiveWords, @ConnectiveWords};
+
+function r = IngWords(yiminus1, yi, xbar, i)
+    r = 1;
+function r = InterrogativeWords(yiminus1, yi, xbar, i)
+    r = 1;
+function r = ExclamatoryWords(yiminus1, yi, xbar, i)
+    r = 1;
+function r = ConnectiveWords(yiminus1, yi, xbar, i)
+    r = 1;
+function r = ConnectiveWords(yiminus1, yi, xbar, i)
+    r = 1;
